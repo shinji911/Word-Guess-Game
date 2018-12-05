18a7835cffc2b1f4086439b97dbbdcca780225df
kshinji911 <kshinji911@gmail.com>
2018-12-04 20:42:11 -0500

    startbtn done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,25 @@
-console.log ("bla")
+//start JS wrap
+
+
+//store elements in variables.
+let startscreen = document.getElementById("startscreen");
+
+let mainscreen = document.getElementById("mainscreen");
+
+let startbtn = document.getElementById("startbtn");
+
+let wincount = document.getElementById("wincount");
+
+let guesscount = document.getElementById("guesscount");
+
+let missedletters = document.getElementById("missedletters");
+
+let wordbox = document.getElementById("wordbox");
+
+
+startbtn.onclick = function() {
+    mainscreen.style.display = "inherit";
+    startscreen.style.display = "none";
+};
+
+//end JS wrap
